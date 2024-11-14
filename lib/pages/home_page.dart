6245fd6c8e215aaf195a77d6ec64e814aca69c16
Ben Rosenthal <50952285+brosenthal3@users.noreply.github.com>
2024-11-14 23:20:27 +0100

    move to desktop and start home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,18 +5,71 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        toolbarHeight: 150,
         title: Text(
           "Pick Your Poison",
           style: GoogleFonts.poppins(
             color:Colors.black,
-            fontSize: 25,
+            fontSize: 40,
             fontWeight: FontWeight.w700,
           ),
         ),
         backgroundColor: Colors.white,
         centerTitle: true,
         elevation: 0.0
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Center(
+                    child: Image(image: AssetImage("../assets/mushroom_concept.png")),
+                  ),
+                  Center(
+                    child: Text(
+                      "Design your own mushrooms",
+                      style: GoogleFonts.poppins(
+                        color:Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.black,
+                                padding: EdgeInsets.symmetric(vertical: 20),
+                              ),
+                              onPressed: () => print("PRESSED"),
+                              child: Text(
+                                "Continue",
+                                style: GoogleFonts.poppins(
+                                  color:Colors.white,
+                                  fontSize: 20,
+                                )
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
