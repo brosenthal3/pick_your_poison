d7586a6ceac55839f56f7b99047213c0ad5e3247
Ben Rosenthal <50952285+brosenthal3@users.noreply.github.com>
2024-12-29 17:24:13 +0100

    handle API from species page

--- lib/pages/species_page.dart
+++ lib/pages/species_page.dart
@@ -12,32 +12,23 @@ class SpeciesPredictionPage extends StatefulWidget {
 }
 
 class _SpeciesPredictionPageState extends State<SpeciesPredictionPage> {
-  late String mushroomFamily = '';
-  late String mushroomSpecies = '';
-
-  String get mushroomSpeciesAsset {
-    return mushroomSpecies.toLowerCase().replaceAll(' ', '-');
-  }
-  String get mushroomFamilyAsset {
-    return mushroomFamily.replaceAll(' ', '_');
-  }
-
-  @override
-  void initState() {
-    asynchInitState();
-  }
-
-  Future<void> asynchInitState() async {
-    final mushroomProvider = Provider.of<MushroomFeaturesProvider>(context, listen: true);
-    final speciesInfo = await mushroomProvider.getSpecies();
-    setState(() {
-      mushroomFamily = speciesInfo['family'] ?? '';
-      mushroomSpecies = speciesInfo['species'] ?? '';
-    });
+  String mushroomSpeciesAsset(String mushroomSpecies, String mushroomFamily) {
+    String species = mushroomSpecies.toLowerCase().replaceAll(' ', '-');
+    String family = mushroomFamily.replaceAll(' ', '_');
+    return "../assets/Species/$family/$species.jpg";
   }
 
   @override
   Widget build(BuildContext context) {
+    final mushroomProvider = Provider.of<MushroomFeaturesProvider>(context, listen: true);
+
+    Future<Map> getSpeciesInfo() async {
+      final speciesInfo = await mushroomProvider.getSpecies();
+      return speciesInfo;
+    }
+
+    late Future<Map> speciesInfo = getSpeciesInfo();
+
     return Scaffold(
       backgroundColor: const Color(0xFFF2EDE2),
       appBar: AppBar(
@@ -47,77 +38,95 @@ class _SpeciesPredictionPageState extends State<SpeciesPredictionPage> {
           child: StandardText("Species Prediction", 25),
         ),
       ),
-      body: (mushroomFamily.isEmpty || mushroomSpecies.isEmpty)
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+      body: Column(
         children: [
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.fromLTRB(40, 0, 40, 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Stack(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(30),
-                        child: ColorFiltered(
-                            colorFilter: ColorFilter.mode(
-                                Colors.black.withOpacity(0.5),
-                                BlendMode.darken),
-                            child: Image(
-                                image: AssetImage("../assets/Species/$mushroomFamilyAsset/$mushroomSpeciesAsset.jpg"),
-                                height: 330,
-                                width: 350,
-                                fit: BoxFit.cover)),
-                      ),
-                      Positioned(
-                        bottom: 20,
-                        left: 25,
-                        child: Text(mushroomFamily,
-                            style: GoogleFonts.poppins(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                            )),
-                      ),
-                      Positioned(
-                        bottom: 50,
-                        left: 25,
-                        child: Text(mushroomSpecies,
-                            style: GoogleFonts.poppins(
-                              color: Colors.white,
-                              fontSize: 25,
-                              fontWeight: FontWeight.w600,
-                            )),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(8, 20, 8, 20),
-                    child: Center(
-                      child: Text(
-                        textAlign: TextAlign.left,
-                        "Amanita phalloides, commonly known as the death cap, is a deadly poisonous basidiomycete fungus, one of many in the genus Amanita. Widely distributed across Europe, A. phalloides forms ectomycorrhizas with various broadleaved trees.",
-                        style: GoogleFonts.poppins(
-                          color: const Color.fromARGB(255, 20, 20, 20),
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
-                        ),
+            FutureBuilder<Map>(
+              future: speciesInfo,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Center(child: Text('No data available'));
+                } else {
+                  final speciesData = snapshot.data!;
+                  final mushroomSpecies = speciesData['name'];
+                  final mushroomFamily = speciesData['family'];
+                  return Expanded(
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(40, 0, 40, 20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Stack(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(30),
+                                child: ColorFiltered(
+                                  colorFilter: ColorFilter.mode(
+                                    Colors.black.withOpacity(0.5),
+                                    BlendMode.darken,
+                                  ),
+                                  child: Image(
+                                    image: AssetImage(mushroomSpeciesAsset(mushroomSpecies, mushroomFamily)),
+                                    height: 330,
+                                    width: 350,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 20,
+                                left: 25,
+                                child: Text(
+                                  mushroomFamily,
+                                  style: GoogleFonts.poppins(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 50,
+                                left: 25,
+                                child: Text(
+                                  mushroomSpecies,
+                                  style: GoogleFonts.poppins(
+                                    color: Colors.white,
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(8, 20, 8, 20),
+                            child: Center(
+                              child: Text(
+                                textAlign: TextAlign.left,
+                                speciesData['description'] ?? 'No description available',
+                                style: GoogleFonts.poppins(
+                                  color: const Color.fromARGB(255, 20, 20, 20),
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                ],
-              ),
+                  );
+                }
+              },
             ),
-          ),
-        ],
-      ),
+          ],
+        )
     );
   }
 }
